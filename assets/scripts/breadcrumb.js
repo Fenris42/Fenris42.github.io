147c7a8e2ccc74ae64fc4af93a8c5b867e48b293
Matt Todd <mtodd447@gmail.com>
2023-12-05 16:50:51 -0800

    breadcrumb

--- assets/scripts/breadcrumb.js
+++ assets/scripts/breadcrumb.js
@@ -1,27 +1,12 @@
-//create breadcrumb
-var breadcrumb = document.createElement('a'); 
-
-//Create home link to breadcrumb
-var text = document.createTextNode("Home");
-breadcrumb.appendChild(text); 
-breadcrumb.href = "https://fenris42.github.io/"; 
-
-//append current page to breadcrumb
+//Get current page
 var path = "";
 var page = "";
 
 path = window.location.pathname;
 page = path.split("/").pop();
 page = page.replace(".html","");
-page = page.replkace("_", " ");
+page = page.replace("_", " ");
 
-//if not the home page then append page
-if (page != ""){
-	page = " / " + page;
-	text = document.createTextNode(page);
-	breadcrumb.appendChild(text); 
-}
-
-//display breadcrumb
-var element = document.getElementById("breadcrumb");
-element.appendChild(breadcrumb); 
+//set breadcrumb to current page
+var element = document.getElementById("currentPage");
+element.innerText = " / " + page;
